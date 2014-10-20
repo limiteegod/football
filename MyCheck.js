275215406532f4a8e52d3937601c54723293bd39
limiteegod <limiteemail@163.com>
2014-10-21 01:38:03 +0800

    快赢481算奖算法

--- MyCheck.js
+++ MyCheck.js
@@ -1,6 +1,7 @@
 var ssq = require("ssq");
+var feo = require("feo");
 
-
+/*
 var gameGrades = [400000000, 100000000, 300000, 20000, 1000, 500];
 var gl = ssq.gl(gameGrades.length);
 gl.setBonus(gameGrades);
@@ -14,4 +15,11 @@ console.log(check.count());
 
 var ticket = {playTypeCode:'00', betTypeCode:'00', numbers:"09,14,17,18,21,25|15"};
 var handle = 'count' + ticket.playTypeCode + ticket.betTypeCode;
-console.log(check[handle](ticket.numbers));
+console.log(check[handle](ticket.numbers));*/
+
+var stringUtil = feo.stringUtil();
+console.log(stringUtil.getIntArray("123,456,321"));
+
+var check = feo.check();
+check.setDrawNum("1,2,3,4");
+console.log(check.getDrawNum());
